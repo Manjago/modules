8e70682abd8e59e33ce10623a2350a2d8e012ffe
Темненков Кирилл Дмитриевич <kdtemnen@mts.ru>
2015-09-11 09:14:05 +0300

    upgrader - fix logical bug

--- upgrader.js
+++ upgrader.js
@@ -8,17 +8,23 @@
 module.exports = function (creep) {
 
     if (creep.carry.energy == 0) {
+        creep.memory.mode = 'LOAD';
         creep.say("u energy");
+        var sources = creep.room.find(FIND_SOURCES);
+        creep.moveTo(sources[0]);
+        creep.harvest(sources[0]);
+    } else if ((creep.carry.energy < creep.carryCapacity) && (creep.memory.mode == 'LOAD')){
+        creep.say("u load");
         var sources = creep.room.find(FIND_SOURCES);
         creep.moveTo(sources[0]);
         creep.harvest(sources[0]);
     }
     else {
-
+        creep.memory.mode = 'WORK';
         if (creep.room.controller) {
             creep.moveTo(creep.room.controller);
             creep.upgradeController(creep.room.controller);
-            creep.say("uprade");
+            creep.say("upgrade");
         } else {
             creep.moveTo(Game.spawns.Spawn1);
             creep.transferEnergy(Game.spawns.Spawn1)
