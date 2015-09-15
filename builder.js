60b65aad7123ae1ec3adf85dd0243b3d29b33350
Темненков Кирилл Дмитриевич <kdtemnen@mts.ru>
2015-09-15 11:01:33 +0300

    FlagBuilder

--- builder.js
+++ builder.js
@@ -26,22 +26,23 @@ module.exports = function (creep) {
             creep.moveTo(targets[0]);
             creep.build(targets[0]);
         } else {
-            var structuresNeedsRepair = findRepo(1500, creep.room);
+            var structuresNeedsRepair = findRepo(3000, creep.room);
 
             if (structuresNeedsRepair.length) {
-                creep.say('rep 1500');
+                creep.say('rep 3000');
                 creep.moveTo(structuresNeedsRepair[0]);
                 creep.repair(structuresNeedsRepair[0]);
             }
             else {
 
-                var structuresNeedsRepair = findRepo(300, creep.room);
+                var structuresNeedsRepair = findRepo(1500, creep.room);
                 if (structuresNeedsRepair.length) {
-                    creep.say('rep 300');
+                    creep.say('rep 1500');
                     creep.moveTo(structuresNeedsRepair[0]);
                     creep.repair(structuresNeedsRepair[0]);
                 } else {
                     creep.say('no work');
+                    creep.moveTo(Game.flags.FlagBuilder);
                 }
 
             }
