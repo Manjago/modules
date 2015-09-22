1a06a17899d2faae514e4a91670591c4fc1a7f27
Темненков Кирилл Дмитриевич <kdtemnen@mts.ru>
2015-09-22 16:56:46 +0300

    Revert "bug fix"
    This reverts commit 0d2e7c82efd4b0753c03c36e22f223054ba3b43c.

--- main.js
+++ main.js
@@ -145,7 +145,7 @@ module.exports.loop = function () {
         }
 
         if (cost != 0) {
-            var spawnParam = decider(options, cost);
+            var spawnParam = decider(cost);
             sp(spawnParam);
         }
 
@@ -156,7 +156,7 @@ module.exports.loop = function () {
         this.cost = cost;
     }
 
-    function decider(options, cost){
+    function decider(cost){
         if (options.harvester < 2) {
             return new SpawnParam(HARVESTER, cost);
         } else if (options.guard < 1) {
